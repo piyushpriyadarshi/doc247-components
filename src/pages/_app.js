0690399903796cd424e87d7246e81a3f9f3649bf
Piyush Priyadarshi <piyush@piyushpriyadarshi.com>
2023-04-21 17:53:58 +0530

    integrated mui library

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
