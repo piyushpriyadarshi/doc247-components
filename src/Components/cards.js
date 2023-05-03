53305aba1eb8b90f5dd969f9a9e3a45f910e2b70
Rahul1 Sharma <rahul1.sharma@inindcrws.com>
2023-05-03 11:19:32 +0530

    Updated: homePage

--- src/Components/cards.js
+++ src/Components/cards.js
@@ -1,67 +1,64 @@
-/* eslint-disable react/jsx-key */
 import * as React from "react";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
-// import ButtonBase from '@material-ui/core/ButtonBase';
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+  Tabs,
+  Tab,
+} from "@mui/material";
 
-import { useRouter } from "next/navigation";
-
-export default function ActionAreaCard({ cardsArray }) {
-  const router = useRouter();
+export default function generalCard({cardsArray,Heading,isFirst}) {
   return (
-    <div
-      style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
-    >
-      {cardsArray.map((item, ind) => {
-        return (
-          <Card
-            sx={{
-              margin: 2,
-              width: { xs: 65, sm: 135 },
-              borderRadius: { xs: 3, sm: 6 },
-            }}
-            onClick={() =>
-              router.push({
-                pathname: "/appointment",
-                state: {
-                  name: "rahul",
-                },
-              })
-            }
-          >
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                image={item.image}
-                alt="green iguana"
-                sx={{ margin: 2, height: { xs: 65, sm: 130 }, margin: 0 }}
-              />
-              <CardContent
-                sx={{
-                  height: { xs: 40, sm: 40 },
-                  padding: 0,
-                  background: "#566984cc",
-                }}
-              >
-                <Typography
-                  sx={{
-                    textAlign: "center",
-                    color: "#00000",
-                    fontSize: { xs: 10, sm: 12 },
-                    fontWeight: 600,
-                    paddingTop: 1,
-                  }}
-                >
-                  {item.name}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        );
-      })}
+    <div style={{marginTop:isFirst?'70px':'14px'}}>
+      <Typography sx={{marginLeft:'12px',fontSize:17,fontWeight:600}}>{Heading}</Typography>
+
+      <Grid
+        item
+        xs={12}
+        sx={{ display: "flex", justifyContent: "space-between" }}
+      >
+        <Tabs
+          variant="scrollable"
+          scrollButtons="auto"
+          aria-label="scrollable auto tabs example"
+          TabIndicatorProps={{
+            style: { transition: "none", display: "none" },
+          }}
+          sx={{ overflowX: 'auto' }}
+          value={1}
+        >
+          {cardsArray.map((item, index) => (
+            <Tab
+              key={index}
+              disableRipple={false}
+              sx={{ margin: 0, padding: "0.5rem" }}
+              label={
+                <Card sx={{ margin: 0, width: { xs: 145, sm: 160 } }}
+                  onClick={() => router.push({
+                    pathname: '/appointment',
+                    state: {
+                      name: 'rahul'
+                    }
+                  })}>
+                    <CardMedia
+                      component="img"
+                      image={item.image}
+                      alt="green iguana"
+                      sx={{ margin: 2, height: { xs: 125, sm: 130 },width: { xs: 145, sm: 160 }, margin: 0 }}
+                    />
+                    <CardContent sx={{ height: { xs: 40, sm: 40 }, width: { xs: 145, sm: 160 },padding: 0, background: '#fff' }} >
+                      <Typography sx={{ textAlign: 'center', color: '#00000', fontSize: { xs: 12, sm: 13 }, fontWeight: 600, paddingTop: 1 }} >
+                        {item.name}
+                      </Typography>
+                    </CardContent>
+                </Card>
+              }
+            />
+          ))}
+        </Tabs>
+      </Grid>
     </div>
   );
 }
